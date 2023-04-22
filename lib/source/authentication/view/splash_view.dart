68f2c44c10a4d74e9bb810cf335362d8d30fd8c8
Johnny <johnny.ayush@rubicotech.in>
2023-04-22 12:58:44 +0530

    Initial Commit

---
+++ lib/source/authentication/view/splash_view.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          "Welcome to News App...",
+          style: TextStyle(
+            color: Colors.red,
+          ),
+        ),
+      ),
+    );
+  }
+}
